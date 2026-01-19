f4e34a8022887d4163bc2af9fe9e0aa626b54123
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2026-01-19 21:20:15 +0100

    update

--- quant/main.cpp
+++ quant/main.cpp
@@ -9,6 +9,7 @@
 
 using namespace tcob;
 namespace io = tcob::io;
+using namespace tcob::gfx;
 
 auto print_error(std::string const& err) -> int
 {
@@ -19,12 +20,33 @@ auto print_error(std::string const& err) -> int
 auto main(int argc, char* argv[]) -> int
 {
     argparse::ArgumentParser program("quant");
-    program.add_argument("input");
-    program.add_argument("output");
-    program.add_argument("ncol")
-        .scan<'i', i32>();
-    program.add_argument("quantizer")
-        .default_value("neuquant");
+    // Positional arguments
+    program.add_argument("input")
+        .help("input image file path")
+        .metavar("INPUT");
+
+    program.add_argument("output")
+        .help("output image file path")
+        .metavar("OUTPUT");
+
+    // Optional arguments with flags
+    program.add_argument("-c", "--colors")
+        .help("number of colors in output palette")
+        .default_value(256)
+        .scan<'i', i32>()
+        .metavar("N");
+
+    program.add_argument("-q", "--quantizer")
+        .help("quantization algorithm")
+        .default_value("neuquant")
+        .choices("neuquant", "octree")
+        .metavar("ALGO");
+
+    program.add_argument("-d", "--dithering")
+        .help("dithering algorithm")
+        .default_value("none")
+        .choices("none", "floyd-steinberg", "ordered", "atkinson")
+        .metavar("ALGO");
 
     auto pl {platform::HeadlessInit()};
 
@@ -36,54 +58,53 @@ auto main(int argc, char* argv[]) -> int
         return 1;
     }
 
-    std::string const src {program.get("input")};
-    std::string const dst {program.get("output")};
-    auto const        ncol {program.get<i32>("ncol")};
-    auto const        quantizer {program.get("quantizer")};
+    std::string const input {program.get<std::string>("input")};
+    std::string const output {program.get<std::string>("output")};
+    i32 const         colors {program.get<i32>("--colors")};
+    std::string const quantizer {program.get<std::string>("--quantizer")};
+    std::string const dithering {program.get<std::string>("--dithering")};
 
-    if (!io::is_file(src)) {
-        return print_error("file not found: " + src);
-    }
+    if (!io::is_file(input)) { return print_error("file not found: " + input); }
 
-    auto in {std::make_shared<io::ifstream>(src)};
+    auto in {std::make_shared<io::ifstream>(input)};
     auto sig {io::magic::get_signature(*in)};
-    if (!sig) {
-        return print_error("invalid file: " + src);
-    }
-
-    using namespace tcob::gfx;
-    std::cout << std::format("converting image to {} colors: {} to {} \n", ncol, src, dst);
+    if (!sig) { return print_error("invalid file: " + input); }
+    std::cout << std::format("converting image to {} colors: {} to {} \n", colors, input, output);
 
     in->seek(0, io::seek_dir::Begin);
 
     image img;
-    if (!img.load(*in, sig->Extension)) {
-        return print_error("error loading image: " + src);
-    }
+    if (!img.load(*in, sig->Extension)) { return print_error("error loading image: " + input); }
 
     auto const& info {img.info()};
-    std::cout << std::format("source info: BPP: {}, Width: {}, Height: {} \n",
-                             (info.Format == image::format::RGBA ? 4 : 3), info.Size.Width, info.Size.Height);
+    std::cout << std::format("source info: BPP: {}, Width: {}, Height: {} \n", (info.Format == image::format::RGBA ? 4 : 3), info.Size.Width, info.Size.Height);
     std::cout << std::format("Old color count:{}\n", img.count_colors());
 
-    stopwatch  sw {stopwatch::StartNew()};
-    auto const run {[&](auto&& quant) {
-        auto const newImg {quant(img)};
-        std::cout << std::format("New color count:{}\nUsed palette:\n", newImg.count_colors());
+    stopwatch sw {stopwatch::StartNew()};
 
-        auto pal {quant.get_palette()};
-        for (i32 i {0}; i < pal.size(); ++i) {
-            std::cout << std::format("{}: {}\n", i, pal[i]);
+    auto const doQuant {[&]<typename T>(T&& quant) { // NOLINT
+        image newImg;
+
+        if (dithering == "ordered") {
+            newImg = ordered_dither {T::GetPalette(img, colors)}(img);
+        } else if (dithering == "atkinson") {
+            newImg = atkinson_dither {T::GetPalette(img, colors)}(img);
+        } else if (dithering == "floyd-steinberg") {
+            newImg = floyd_steinberg_dither {T::GetPalette(img, colors)}(img);
+        } else {
+            newImg = quant(img);
         }
 
+        std::cout << std::format("New color count:{}\nUsed palette:\n", newImg.count_colors());
         auto const ms {sw.elapsed_milliseconds()};
-        if (newImg.save(dst)) {
+
+        if (newImg.save(output)) {
             std::cout << std::format("done in {}ms!\n", ms);
             return 0;
         }
-        return print_error("error saving image: " + dst);
+        return print_error("error saving image: " + output);
     }};
 
-    if (quantizer == "neuquant") { return run(neuquant {ncol}); }
-    return run(octree_quantizer {ncol});
+    if (quantizer == "neuquant") { return doQuant(neuquant {colors}); }
+    return doQuant(octree_quantizer {colors});
 }
